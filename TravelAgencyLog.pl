27f4d00af8e5dfb2d6788d2a3ed62b3b73207193
PabloNaranjo78 <pablonaranjo5@estudiantec.cr>
2022-03-19 02:18:51 -0600

    Se añaden funciones iniciales y elementos de gramática

---
+++ TravelAgencyLog.pl
@@ -0,0 +1,6 @@
+iniciar():-
+    write("Bienvenido a TravelAgencyLog la mejor logica de llegar a su destino"),nl,
+    write("Por Favor indiqueme cual es el origen de su vuelo"),nl,
+    readln(X,_,_,_,lowercase),
+    write(X).
+
---
+++ grafo.pl
@@ -0,0 +1,8 @@
+arco(copa, cm404, sanJose, chile, 1, ambas, 301).
+arco(united,cs234, kenya, mexico, 1, ambas, 300).
+arco(delta, hh232, qatar, colombia, 1, ambas, 300).
+arco(spirit, op342, nicaragua, argentina, 1, ambas, 300).
+arco(avianca, nm231, honduras, sanJose, 1, ambas, 300).
+arco(emirates, po98N, peru, usa, 1, ambas, 300).
+arco(jetblue, se231, chile, espana, 1, ambas, 300).
+arco(aeromexico, lo231, guatemala, sudamerica, 1, ambas, 300).
